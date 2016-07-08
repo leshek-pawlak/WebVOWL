697d53c4ac9d331a77daa771b6cc3ca3bff0dcd3
giemzap <pawel.giemza@contractors.roche.com>
2016-07-08 11:02:19 +0200

    Added possibility to define property backgroundColor in model

--- src/webvowl/js/parser.js
+++ src/webvowl/js/parser.js
@@ -302,7 +302,8 @@ module.exports = function (graph) {
 						.subproperties(element.subproperty)
 						.superproperties(element.superproperty)
 						// .type(element.type) Ignore, because we predefined it
-						.iri(element.iri);
+						.iri(element.iri)
+						.backgroundColor(element.backgroundColor);
 
 					// adding property position
 					if (element.pos) {
