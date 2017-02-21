277e77111b5e25a1c751d2629bb6482008ea03d8
Leszek Pawlak <leszek.pawlak@contractors.roche.com>
2017-02-21 18:05:20 +0100

    Move title to top for UML structure

--- src/app/js/menu/structuresMenu.js
+++ src/app/js/menu/structuresMenu.js
@@ -71,12 +71,34 @@ module.exports = function (graph) {
     function render() {
       d3.selectAll('.elements-to-change').each(function () {
         var element = d3.select(this);
+        var textElement = d3.select(getClosestTextElement(element));
         if (structure === 'rect') {
+          // change circle to rect
           element.property('outerHTML', element.property('outerHTML').replace(/circle/g, 'rect'));
+          // save old 'y' value as 'data-y'
+          if (!textElement.attr('data-y')) {
+            textElement.attr('data-y', textElement.attr('y'));
+          }
+          // move text to the top of rect. like in UML structure.
+          textElement.attr('y', -1 * parseInt(element.attr('r')) + 5 + 'px');
         } else {
+          if (textElement.attr('data-y')) {
+            // restore old 'y' value from 'data-y'
+            textElement.attr('y', textElement.attr('data-y'));
+          }
+          // change rect into circle back again
           element.property('outerHTML', element.property('outerHTML').replace(/rect/g, 'circle'));
         }
       });
+    }
+
+    function getClosestTextElement(element) {
+      var node = element.node();
+      while (typeof node === 'object' && node.nodeName !== 'text') {
+        node = node.nextElementSibling;
+      }
+
+      return node;
     }
 
     function prepareCircles() {
