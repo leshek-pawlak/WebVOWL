4576f26a4df448991e8d37486edeae0f026fef90
Leszek Pawlak <leszek.pawlak@contractors.roche.com>
2017-03-23 12:10:48 +0100

    Custom property could be any HTML

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -36,6 +36,13 @@ module.exports = function () {
 		tagFilter,
 		setOperatorFilter;
 
+	// it let us append every html string
+	d3.selection.prototype.appendHTML = function(HTMLString) {
+			return this.select(function() {
+					return this.appendChild(document.importNode(new DOMParser().parseFromString(HTMLString, 'text/html').body.childNodes[0], true));
+			});
+	};
+
 	app.overrideOptions = function(overridingOptions) {
 		options.rewriteFrom(overridingOptions);
 	};
