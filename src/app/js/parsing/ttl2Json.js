5d58eeabc685d865d71b90649f7ef8ab3b650a36
Leszek Pawlak <leszek@bitnoi.se>
2017-05-10 12:26:27 +0200

    Improvments for extract ttl to json

--- src/app/js/parsing/ttl2Json.js
+++ src/app/js/parsing/ttl2Json.js
@@ -7,25 +7,27 @@ module.exports = function() {
 
   // application
   function parseTtl(ttl) {
-    // TODO TTL -> JSON
-    var store = [];
-    var parser = N3.Parser();
-    var tmp = {}
+    var parser = N3.Parser(),
+      store = [],
+      tmp = {},
+      finalJson = {};
     parser.parse(ttl, function(error, triple, prefixes) {
       if (triple) {
+        // save triples as js objects
         if (isEmpty(tmp)) {
           tmp._uri = triple.subject;
-          tmp[triple.predicate] = triple.object;
-        } else if (triple.subject === tmp._uri) {
-          tmp[triple.predicate] = triple.object;
-        } else {
+        } else if (triple.subject !== tmp._uri) {
           store.push(tmp);
-          tmp = {};
+          tmp = {
+            _uri: triple.subject
+          };
         }
-        // if (N3Util.isIRI(subject)) {}
+        tmp[triple.predicate] = triple.object;
       } else {
+        // replace all keys in objects prefixes defined in turtle file
         var newStore = replacePrefixesInStore(store, prefixes);
-        console.log(newStore);
+        finalJson = createNestedStructure(newStore);
+        console.log(finalJson);
       }
     });
     // parsing JSON
@@ -70,20 +72,74 @@ module.exports = function() {
       newStore.push({});
       Object.keys(store[i]).map(function(storeObjectKey, index) {
         var replacedKey = storeObjectKey;
+        var value = store[i][storeObjectKey];
         Object.keys(prefixes).map(function(prefixKey, index) {
           var iriToReplace = prefixes[prefixKey];
           if (storeObjectKey.indexOf(iriToReplace) > -1) {
             replacedKey = storeObjectKey.replace(iriToReplace, prefixKey + '_');
           }
+          if (storeObjectKey.indexOf('type') > -1 && value.indexOf(iriToReplace) > -1) {
+            value = value.replace(iriToReplace, prefixKey + ':');
+          }
         });
-        newStore[i][replacedKey] = store[i][storeObjectKey];
+        newStore[i][replacedKey] = value;
       });
     }
-    // for (var i = 0; i < prefixes.length; i++) {
-    //   prefixes[i]
-    // }
 
     return newStore;
+  }
+
+  function createNestedStructure(store) {
+    var json = {};
+    for (var i = 0; i < store.length; i++) {
+      json = findObjectToPut(store[i], json);
+    }
+
+    return json;
+  }
+
+  function findObjectToPut(element, json) {
+    var typeKey = getPathToPutObject('type', element);
+    var typeOfElement = _.snakeCase(element[typeKey[0]]);
+    if (isEmpty(json)) {
+      element.list = {
+        owl_class: [],
+        owl_datatype_property: [],
+        owl_object_property: []
+      };
+      json[typeOfElement] = element;
+    } else if (typeOfElement) {
+      var arrayPath = getPathToPutObject(typeOfElement, json);
+      var obj = json;
+      console.log('arrayPath', arrayPath);
+      for (var i = arrayPath.length - 1; i > 0; --i) {
+        obj = obj[arrayPath[i]];
+      }
+      console.log('arrayPath[0]', arrayPath[0]);
+      if (!obj[arrayPath[0]] || !obj[arrayPath[0]].length) {
+        obj[arrayPath[0]] = [];
+      }
+      obj[arrayPath[0]].push(element);
+    }
+
+    return json;
+  }
+
+  function getPathToPutObject(key, element, res) {
+    res = res ? res : [];
+    Object.keys(element).map(function(elementKey, index) {
+      if (typeof res === 'object' && typeof element[elementKey] === 'object') {
+        var recusiveKey = getPathToPutObject(key, element[elementKey], res);
+        if (recusiveKey.length > 0 && !element[recusiveKey[recusiveKey.length - 1]]) {
+          res.push(elementKey);
+        }
+      }
+      if (elementKey.indexOf(key) > -1) {
+        res.push(elementKey);
+      }
+    });
+    console.log('res: ', res);
+    return res;
   }
 
   return parseTtl;
