27b5ff05f6110acd15781b2dfb5a478b9d02e7d9
Leszek Pawlak <leszek@bitnoi.se>
2017-05-15 20:03:22 +0200

    Fix merge triples to json

--- src/app/js/parsing/ttl2Json.js
+++ src/app/js/parsing/ttl2Json.js
@@ -11,25 +11,45 @@ module.exports = function() {
       store = [],
       tmp = {},
       parsed = parser.parse(ttl);
+    console.log('1. ttl parsed by N3: ', parsed);
     // NOTE don't use N3 parser with callback. It started to be async and breaks return value!!
     for (var i = 0; i < parsed.length; ++i) {
       // save triples as js objects
       if (isEmpty(tmp)) {
         tmp._uri = parsed[i].subject;
-      } else if (parsed[i].subject !== tmp._uri) {
+      } else if (parsed[i].subject !== tmp._uri || i === parsed.length - 1) {
         store.push(tmp);
         tmp = {
           _uri: parsed[i].subject,
           _hash: getHash(parsed[i].subject)
         };
       }
-      tmp[parsed[i].predicate] = parsed[i].object;
+      var tripleObject = getValueInType(parsed[i].object);
+      // if we want to overwrite variable lets create an array and push them variables
+      if (typeof tmp[parsed[i].predicate] === 'string') {
+        var val = tmp[parsed[i].predicate];
+        tmp[parsed[i].predicate] = [val];
+      }
+      if (typeof tmp[parsed[i].predicate] === 'object') {
+        tmp[parsed[i].predicate].push(tripleObject);
+      } else {
+        tmp[parsed[i].predicate] = tripleObject;
+      }
     }
+    console.log('2. get triple.object in write type, create "key":"value" relations and add uri and hash: ', store);
     var newStore = replacePrefixesInStore(store, parser._prefixes);
+    console.log('3. replace uri\'s by prefixes: ', newStore);
     // parsing JSON to graph valid JSON
     return parseJson(createNestedStructure(newStore));
   }
 
+  function getValueInType(value) {
+    var N3Util = N3.Util;
+    if (N3Util.isLiteral(value)) {
+      return N3Util.getLiteralValue(value);
+    }
+    return value;
+  }
 
   function getHash(url) {
     return url.substring(url.indexOf('#') + 1);
@@ -67,10 +87,10 @@ module.exports = function() {
     var newStore = [];
     for (var i = 0; i < store.length; i++) {
       newStore.push({});
-      Object.keys(store[i]).map(function(storeObjectKey, index) {
+      Object.keys(store[i]).map(function(storeObjectKey) {
         var replacedKey = storeObjectKey;
         var value = store[i][storeObjectKey];
-        Object.keys(prefixes).map(function(prefixKey, index) {
+        Object.keys(prefixes).map(function(prefixKey) {
           var iriToReplace = prefixes[prefixKey];
           if (storeObjectKey.indexOf(iriToReplace) > -1) {
             replacedKey = storeObjectKey.replace(iriToReplace, prefixKey + '_');
@@ -91,7 +111,7 @@ module.exports = function() {
     for (var i = 0; i < store.length; i++) {
       json = findObjectToPut(store[i], json);
     }
-
+    console.log('4. nested json ready to send to parseJSON(): ', json);
     return json;
   }
 
@@ -106,15 +126,20 @@ module.exports = function() {
         object_property: []
       };
       json[typeOfElement] = element;
-    } else if (typeOfElement.indexOf('class') > -1) {
-      var arrayPath = getPathToPutObject(typeOfElement, json);
+    } else {
+      var arrayPath = getPathToPutObject(typeOfElement, json, [], element);
       var obj = json;
       for (var i = arrayPath.length - 1; i > 0; --i) {
         obj = obj[arrayPath[i]];
       }
       if (obj[arrayPath[0]]) {
+        var indexInArray = obj[arrayPath[0]].indexOf(element._uri);
         if (typeof obj[arrayPath[0]] === 'string') {
           obj[arrayPath[0]] = [];
+        }
+        // remove strings labels when we add object instead
+        if (indexInArray > -1) {
+          obj[arrayPath[0]].splice(indexInArray, 1);
         }
         obj[arrayPath[0]].push(element);
       }
@@ -123,16 +148,22 @@ module.exports = function() {
     return json;
   }
 
-  function getPathToPutObject(key, element, res) {
+  function getPathToPutObject(key, element, res, rootElement) {
     res = res ? res : [];
     Object.keys(element).map(function(elementKey, index) {
       if (typeof res === 'object' && typeof element[elementKey] === 'object') {
-        var recusiveKey = getPathToPutObject(key, element[elementKey], res);
-        if (recusiveKey.length > 0 && !element[recusiveKey[recusiveKey.length - 1]]) {
+        var recusiveKey = getPathToPutObject(key, element[elementKey], res, rootElement);
+        // 1. if there is already some part of path in the recusiveKey array
+        // 2. if the elementKey is not from the level we added last key
+        // 3. if the key is already in the array
+        if (recusiveKey.length > 0 && !element[recusiveKey[recusiveKey.length - 1]] && res.indexOf(elementKey) === -1) {
           res.push(elementKey);
         }
       }
-      if (res.length < 1 && elementKey.indexOf(key.replace(/^.+_/i, '')) > -1) {
+      // 1. if the res is empty array
+      // 2. elementKey ends with finding word
+      // 3. if it's label we need to match _uri
+      if (res.length < 1 && elementKey.endsWith(key.replace(/^.+_/i, '')) && (key.indexOf('label') === -1 || rootElement._uri.indexOf(element._uri) > -1)) {
         res.push(elementKey);
       }
     });
